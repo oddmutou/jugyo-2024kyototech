05cc40babef1703fc16fa959c73d9bee9d7b2c6f
KITAGAWA Nao <muto@muto-ct.jp>
2024-11-30 17:28:19 +0900

    後期第5回授業 会員登録画面 実装

---
+++ koki_05/public/signup.php
@@ -0,0 +1,42 @@
+<?php
+// DBに接続
+$dbh = new PDO('mysql:host=mysql;dbname=kyototech', 'root', '');
+
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+  // POSTで name と email と password が送られてきた場合はDBへの登録処理をする
+  // insertする
+  $insert_sth = $dbh->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+  $insert_sth->execute([
+    ':name' => $_POST['name'],
+    ':email' => $_POST['email'],
+    ':password' => $_POST['password'],
+  ]);
+
+  // 処理が終わったら完了画面にリダイレクト
+  header("HTTP/1.1 302 Found");
+  header("Location: /signup_finish.php");
+  return;
+}
+?>
+<h1>会員登録</h1>
+
+<!-- 登録フォーム -->
+<form method="POST">
+  <!-- input要素のtype属性は全部textでも動くが、適切なものに設定すると利用者は使いやすい -->
+  <label>
+    名前:
+    <input type="text" name="name">
+  </label>
+  <br>
+  <label>
+    メールアドレス:
+    <input type="email" name="email">
+  </label>
+  <br>
+  <label>
+    パスワード:
+    <input type="password" name="password" minlength="6" autocomplete="new-password">
+  </label>
+  <br>
+  <button type="submit">決定</button>
+</form>
