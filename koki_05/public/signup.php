c72591af7e03ef461ccdfa95cfcf960dc617919e
KITAGAWA Nao <muto@muto-ct.jp>
2024-12-07 17:51:45 +0900

    後期第5回 演習1

--- koki_05/public/signup.php
+++ koki_05/public/signup.php
@@ -4,6 +4,20 @@ $dbh = new PDO('mysql:host=mysql;dbname=kyototech', 'root', '');
 
 if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
   // POSTで name と email と password が送られてきた場合はDBへの登録処理をする
+
+  // 既に同じメールアドレスで登録された会員が存在しないか確認する
+  $select_sth = $dbh->prepare("SELECT * FROM users WHERE email = :email ORDER BY id DESC LIMIT 1");
+  $select_sth->execute([
+    ':email' => $_POST['email'],
+  ]);
+  $user = $select_sth->fetch();
+  if (!empty($user)) {
+    // 存在した場合 エラー用のクエリパラメータ付き会員登録画面にリダイレクトする
+    header("HTTP/1.1 302 Found");
+    header("Location: ./signup.php?duplicate_email=1");
+    return;
+  }
+
   // insertする
   $insert_sth = $dbh->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
   $insert_sth->execute([
@@ -40,3 +54,9 @@ if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password
   <br>
   <button type="submit">決定</button>
 </form>
+
+<?php if(!empty($_GET['duplicate_email'])): ?>
+<div style="color: red;">
+  入力されたメールアドレスは既に使われています。
+</div>
+<?php endif; ?>
